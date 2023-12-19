74d347214ef7d675be2a7e5885b7987148dcc4d2
Robert Williamson <robert11122002@gmail.com>
2023-12-19 12:51:53 +0000

    cleaner usage, each command doesnt print full path

--- src/usage.nim
+++ src/usage.nim
@@ -3,18 +3,19 @@ import std/os
 
 let
     exePath = getAppFilename()
+    exeName = exePath.splitFile().name
 
 proc printUsage(): void =
     echo fmt"""Usage: {exePath} <options> <?entry-file>
-    run | r <options>   {exePath} run [-e | --entry] [Main]
+    run | r <options>   {exeName} run [-e | --entry] [Main]
                         run the project, optionally provide the name of the entry
-    build | b           {exePath} build
+    build | b           {exeName} build
                         build the project, and output the class files to the bin dir
-    new | n             {exePath} new [-e | --entry] [MyProject]
+    new | n             {exeName} new [-e | --entry] [MyProject]
                         create a new project in its own directory with a specified name, has a default name if none provided
-    init | i            {exePath} init [-e | --entry] [MyProject]
+    init | i            {exeName} init [-e | --entry] [MyProject]
                         initialize the current directory with a specified entry point
-    index | x           {exePath} index
+    index | x           {exeName} index
                         index the current project
 """
 
